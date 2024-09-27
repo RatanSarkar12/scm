03311ba0ffd3295a2d51f7baaf95ae22405fa6eb
Ratan sarkar <sarkarratany618@gmail.com>
2024-09-28 05:01:52 +0530

    initial commit

---
+++ src/test/java/com/ratan/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratan.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
